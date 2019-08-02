26e1036fe2d298dd4d8796f5b8cf57a7642659e8
Stanly Rodriguez <ibestanly@gmail.com>
2019-08-02 16:41:51 -0400

    working api

---
+++ main.js
@@ -0,0 +1,39 @@
+const images = document.getElementById("images");
+const searchForm = document.getElementById("searchForm");
+
+const search = document.getElementById("search");
+
+const submit = document.getElementById("submit");
+
+searchForm.addEventListener("submit", function(e) {
+  images.innerHTML = "";
+  e.preventDefault();
+  getData(search.value);
+});
+
+function getData(query) {
+  //   console.log(query);
+  const uriQuery = encodeURI(query);
+
+  fetch(
+    "http://api.giphy.com/v1/gifs/search?q=" +
+      uriQuery +
+      "&api_key=w0U7qBZR2giDBOCdkUcoaElRT4jKrmEQ&limit=5"
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      renderData(myJson);
+      //   console.log(JSON.stringify(myJson));
+    });
+}
+function renderData(data) {
+  console.log(data.data);
+  for (let i = 0; i < data.data.length; i++) {
+    const img = document.createElement("img");
+    img.src = data.data[i].images.original.url;
+
+    images.appendChild(img);
+  }
+}
